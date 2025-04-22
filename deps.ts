4aac332f42a6806c6333f4012f4dcea7ea904aaf
Zicklag <zicklag@katharostech.com>
2025-04-22 12:22:36 -0500

    refactor: convert to a JSR package.

--- deps.ts
+++
@@ -1,1 +0,0 @@
-export { getLogger } from "https://deno.land/std@0.150.0/log/mod.ts";
--- test_deps.ts
+++
@@ -1,5 +0,0 @@
-export * from "https://deno.land/std@0.150.0/testing/asserts.ts";
-
-export { describe, it } from "https://deno.land/std@0.150.0/testing/bdd.ts";
-
-export * from "https://deno.land/std@0.150.0/log/mod.ts";
